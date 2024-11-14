de4857c7060e609bb21baee40cac39a565839e47
Ronney Rocha <ronneyrv@gmail.com>
2024-11-14 17:41:58 -0300

    feat: clean in the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="pt-Br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
