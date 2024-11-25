ca74eb15a84c8a76e4619ffed479c41b45e128f0
Ronney Rocha <ronneyrv@gmail.com>
2024-11-25 09:24:37 -0300

    feat: add tailwind css extended theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "var(--color-text)",
+        error: "var(--color-error)",
+        paper: "var(--color-paper)",
+        primary: "var(--color-primary)",
+        background: "var(--color-background)",
+        "primary-contrast": "var(--color-primary-contrast)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
