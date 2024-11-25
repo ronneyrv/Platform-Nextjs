8e318ef708eef94a68c7e1acbc19858ac647b55e
Ronney Rocha <ronneyrv@gmail.com>
2024-11-25 11:27:09 -0300

    feat: add components header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Header } from "@/components/header/Header";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +13,8 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-Br">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Header/>
         {children}
       </body>
     </html>
