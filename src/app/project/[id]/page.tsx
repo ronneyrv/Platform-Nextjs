a548d2b0eb2c82becda794ff6626cb1e647f9044
Ronney Rocha <ronneyrv@gmail.com>
2024-12-03 19:04:54 -0300

    refactor: clear code page projects

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,15 +1,11 @@
 import { ProjectHeader } from "@/components/project-header/ProjectHeader";
 import { Metadata } from "next";
 
-interface Props {
-    params: {id: string}
-}
-
 export const metadata: Metadata = {
     title: "Gerenciador de Tarefas",
   };
 
-export default function PageProjectDetal({params}: Props) {
+export default function PageProjectDetal() {
     return (
         <main className="mt-8 flex justify-center">
             <div className="w-full min-[880px]:max-w-[880px]">
