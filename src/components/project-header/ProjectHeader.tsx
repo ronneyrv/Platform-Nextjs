502c67f3ae9c596f36b96fab4afc74d036837901
Ronney Rocha <ronneyrv@gmail.com>
2024-12-06 14:29:57 -0300

    refactor: slider of swiper corrected

--- src/components/project-header/ProjectHeader.tsx
+++ src/components/project-header/ProjectHeader.tsx
@@ -1,96 +1,48 @@
 "use client";
-import { SwiperProps, SwiperSlide } from "swiper/react";
-import Slider from "../card/Slider";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Autoplay,
+  EffectCoverflow,
+  EffectCube,
+} from "swiper/modules";
 import Image from "next/image";
 
 import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/effect-cube";
 
 export const ProjectHeader = () => {
-  const settings: SwiperProps = {
-    spaceBetween: 0,
-    slidesPerView: 1,
-    navigation: true,
-    effect: "cards",
-    grabCursor: true,
-    pagination: {
-      clickable: false,
-    },
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: true,
-    },
-  };
-
-  const settings2: SwiperProps = {
-    spaceBetween: 0,
-    slidesPerView: 1,
-    navigation: false,
-    
-    grabCursor: true,
-    pagination: {
-      clickable: true,
-    },
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: true,
-    },
-  };
-
   return (
     <>
-        <div className="block pt-10 sm:hidden">
-        <Slider settings={settings2}>
-          <SwiperSlide >
-            <Image
-              src="/images/gerenciador-tarefas/gerenciador-tarefa5.png"
-              alt="Picture 1"
-              width={300}
-              height={400}
-            />
-          </SwiperSlide>
-          <SwiperSlide >
-            <Image
-              src="/images/gerenciador-tarefas/gerenciador-tarefa2.png"
-              alt="Picture 2"
-              width={300}
-              height={500}
-            />
-          </SwiperSlide>
-          <SwiperSlide >
-            <Image
-              src="/images/gerenciador-tarefas/gerenciador-tarefa3.png"
-              alt="Picture 3"
-              width={300}
-              height={400}
-            />
-          </SwiperSlide>
-          <SwiperSlide >
-            <Image
-              src="/images/gerenciador-tarefas/gerenciador-tarefa4.png"
-              alt="Picture 4"
-              width={300}
-              height={400}
-            />
-          </SwiperSlide>
-          <SwiperSlide >
-            <Image
-              src="/images/gerenciador-tarefas/gerenciador-tarefa.png"
-              alt="Picture 5"
-              width={500}
-              height={500}
-            />
-          </SwiperSlide>
-        </Slider>
-        </div>
-        
-        <div className="hidden sm:block pt-20">
-        <Slider settings={settings}>
+      <div className="block w-full justify-center p-10 sm:hidden">
+        <Swiper
+          modules={[Autoplay, EffectCube]}
+          spaceBetween={0}
+          slidesPerView={1}
+          loop={true}
+          navigation={false}
+          effect={"cube"}
+          grabCursor={true}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          cubeEffect={{
+            shadow: false,
+            slideShadows: true,
+            shadowOffset: 0,
+            shadowScale: 0.94,
+          }}
+        >
           <SwiperSlide>
             <Image
               src="/images/gerenciador-tarefas/gerenciador-tarefa5.png"
               alt="Picture 1"
               width={300}
-              height={500}
+              height={400}
+              className="block mx-auto "
             />
           </SwiperSlide>
           <SwiperSlide>
@@ -98,7 +50,8 @@ export const ProjectHeader = () => {
               src="/images/gerenciador-tarefas/gerenciador-tarefa2.png"
               alt="Picture 2"
               width={300}
-              height={500}
+              height={400}
+              className="block mx-auto"
             />
           </SwiperSlide>
           <SwiperSlide>
@@ -106,7 +59,8 @@ export const ProjectHeader = () => {
               src="/images/gerenciador-tarefas/gerenciador-tarefa3.png"
               alt="Picture 3"
               width={300}
-              height={500}
+              height={400}
+              className="block mx-auto"
             />
           </SwiperSlide>
           <SwiperSlide>
@@ -114,21 +68,89 @@ export const ProjectHeader = () => {
               src="/images/gerenciador-tarefas/gerenciador-tarefa4.png"
               alt="Picture 4"
               width={300}
-              height={500}
+              height={400}
+              className="block mx-auto"
             />
           </SwiperSlide>
           <SwiperSlide>
             <Image
               src="/images/gerenciador-tarefas/gerenciador-tarefa.png"
               alt="Picture 5"
-              width={500}
-              height={500}
+              width={400}
+              height={400}
+              className="block mx-auto"
             />
           </SwiperSlide>
-        </Slider>
+        </Swiper>
       </div>
 
-      <div className="flex flex-col gap-2 p-8">
+      <div className="hidden pt-20 sm:block">
+        <Swiper
+          modules={[Navigation, Autoplay, EffectCoverflow]}
+          spaceBetween={0}
+          slidesPerView={3}
+          loop={true}
+          navigation={false}
+          effect={"coverflow"}
+          grabCursor={true}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          coverflowEffect={{
+            rotate: 100,
+            stretch: 50,
+            depth: 100,
+            modifier: 2,
+            slideShadows: true,
+          }}
+          className="flex justify-center"
+        >
+          <SwiperSlide>
+            <Image
+              src="/images/gerenciador-tarefas/gerenciador-tarefa5.png"
+              alt="Picture 1"
+              width={300}
+              height={500}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/images/gerenciador-tarefas/gerenciador-tarefa2.png"
+              alt="Picture 2"
+              width={300}
+              height={500}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/images/gerenciador-tarefas/gerenciador-tarefa3.png"
+              alt="Picture 3"
+              width={300}
+              height={500}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/images/gerenciador-tarefas/gerenciador-tarefa4.png"
+              alt="Picture 4"
+              width={300}
+              height={500}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/images/gerenciador-tarefas/gerenciador-tarefa.png"
+              alt="Picture 5"
+              width={500}
+              height={500}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+
+      <div className="flex flex-col gap-2 p-8 pt-0">
         <h1 className="font-extrabold text-xl">Gerenciador de Tarefas</h1>
         <p className="text-justify overflow-x-auto">
           O Gerenciador de Tarefas é uma aplicação web desenvolvida para
@@ -153,9 +175,15 @@ export const ProjectHeader = () => {
         </p>
 
         <div>
-          <button 
-          onClick={() => window.open('https://gerenciador-de-tarefas-liart.vercel.app', '_blank')}
-          className="py-2 px-4 bg-paper rounded-full">
+          <button
+            onClick={() =>
+              window.open(
+                "https://gerenciador-de-tarefas-liart.vercel.app",
+                "_blank"
+              )
+            }
+            className="py-2 px-4 bg-paper rounded-full"
+          >
             Visualizar
           </button>
         </div>
